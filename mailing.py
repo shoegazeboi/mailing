dc0fa07b1953cbdf3d49c46e1688e5958122470d
shoegaze-root <2003dims@gmail.com>
2020-07-05 09:19:55 +0300

    init

--- mailing.py
+++ mailing.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
+
 import PySimpleGUI as SG
 import sqlite3
 from time import sleep
